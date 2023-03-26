ef383575dcd65270b5932c58dc72f0d6efd84268
seoyoung81 <seosue0801@gmail.com>
2023-03-26 22:23:05 +0900

    delete

--- todo_list/todos/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class TODO(models.Model):
-    title = models.CharField(max_length=10)
-    content = models.TextField()
-    image = models.ImageField(blank=True)
